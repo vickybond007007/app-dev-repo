46a312839aba5cd844e20c315792b12e7e319692
vickybond007007 <vickybond007007@gmail.com>
2026-05-03 16:28:40 +0530

    Add instance variables and constructor to App class

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,6 +4,19 @@ package org.example;
  * Hello world!
  */
 public class App {
+
+    public int instancevar1;
+    public String instancevar2;
+    
+    public App(int instancevar1, String instancevar2){
+        this.instancevar1 = instancevar1;
+        this.instancevar2 = instancevar2;
+    }
+
+    public int calculateSum(int a, int b){
+        return a + b;
+    }
+    
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World!--new chnages");
